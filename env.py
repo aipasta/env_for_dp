f79f6e56d628a1b2087828d0d94b1da0bb748e80
aipasta <aipasta1221@gmail.com>
2021-10-04 22:38:16 +0100

    Add environment

---
+++ env.py
@@ -0,0 +1,177 @@
+from typing import List, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
+from enum import Enum, IntEnum
+
+map_data_1 = '''\
+######
+#X   #
+#    #
+#    #
+#   X#
+######
+'''
+map_data_2 = '''\
+###################
+#                X#
+#   ###########   #
+#   #         #   #
+#   # ####### #   #
+#   # #     # #   #
+#     #  #  #     #
+#        #        #
+#                 #
+###             ###
+#                 #
+###################
+'''
+
+
+class Action(IntEnum):
+  LEFT = 0
+  DOWN = 1
+  RIGHT = 2
+  UP = 3
+
+
+class CellType(Enum):
+  WALL = 1
+  EMPTY_CELL = 2
+  GOAL_CELL = 3
+
+
+def symbol_to_cell_type(symbol):
+  return {'#': CellType.WALL,
+          ' ': CellType.EMPTY_CELL,
+          'X': CellType.GOAL_CELL}[symbol]
+
+
+class Environment():
+  """Class for grid world environment
+
+  Attributes:
+    map: The map consists of `cell_type`.
+    states: States the environment has. States are represented as an int (i.e.,
+      [0, num_state-1]).
+    state_to_map: The mapping between state and map (e.g, state -> (row, col)).
+    map_to_state: The mapping between map and state.
+      (e.g, map_to_state[row][col] -> state).
+    num_state: Size of state space.
+    num_action: Size of action space.
+    map_width: The width of map which includes cell and wall.
+    map_height: The height of map which includes cell and wall.
+  """
+
+  def __init__(self, map_data: str):
+    self.map = self._load_map_data(map_data)
+    self.state_to_map, self.map_to_state = self._get_mapping_state_and_map()
+    self.states = list(range(self.num_state))
+    self.num_action = 4  # LEFT, DOWN, RIGHT, UP
+
+  @property
+  def num_state(self):
+    return len(self.state_to_map)
+
+  @property
+  def map_width(self):
+    return len(self.map[0])
+
+  @property
+  def map_height(self):
+    return len(self.map)
+
+  def _load_map_data(self, map_data: str) -> List:
+    map = []
+    for line in map_data.splitlines():
+      cell_row = []
+      for c in line:
+        cell_row.append(symbol_to_cell_type(c))
+      map.append(cell_row)
+    return map
+
+  def _get_mapping_state_and_map(self) -> Tuple[List, List]:
+    state_to_map = []
+    map_to_state = [[None]*self.map_width for _ in range(self.map_height)]
+
+    for row in range(self.map_height):
+      for col in range(self.map_width):
+        if not self.map[row][col] == CellType.WALL:
+          state_to_map.append((row, col))
+          map_to_state[row][col] = len(state_to_map)-1
+
+    return state_to_map, map_to_state
+
+  def _get_cell_by_state(self, state: int):
+    row, col = self.state_to_map[state]
+    return self.map[row][col]
+
+  def _get_next_state(self, state: int, action: int) -> int:
+    row, col = self.state_to_map[state]
+
+    # If the agent achieves the goal, the episode terminates. So there is no
+    # next state.
+    if self.map[row][col] == CellType.GOAL_CELL:
+      return state
+
+    if action == Action.LEFT:
+      col = col - 1
+    elif action == Action.DOWN:
+      row = row + 1
+    elif action == Action.RIGHT:
+      col = col + 1
+    elif action == Action.UP:
+      row = row - 1
+    # If the agent is blocked by wall, it remain at the same state.
+    if self.map[row][col] == CellType.WALL:
+      return state
+    else:
+      return self.map_to_state[row][col]
+
+  def get_transition_probability(self, state: int, action: int,
+                                 next_state: int) -> float:
+    if self._get_next_state(state, action) == next_state:
+      return 1.0
+    else:
+      return 0.0
+
+  def get_reward(self, state: int, action: int):
+    """Return the reward for given sate and action.
+
+    The reward for goal state is 0 and otherwise -1.
+    """
+    del action  # Not used in this environment.
+    if self._get_cell_by_state(state) == CellType.GOAL_CELL:
+      return 0
+    else:
+      return -1.0
+
+  def value_function_with_structure(self, value_function: List) -> np.ndarray:
+    arr = np.empty((self.map_height-2, self.map_width-2)) * np.nan
+    for state in range(len(value_function)):
+      row, col = self.state_to_map[state]
+      arr[row-1, col-1] = value_function[state]
+    return arr
+
+  def pretty_print_value_function(self, value_function: List):
+    arr = self.value_function_with_structure(value_function)
+
+    with np.printoptions(precision=2, suppress=True):
+      print(np.reshape(np.array(arr), (self.map_height-2, self.map_width-2)))
+
+  def plot_value_function(self, value_function):
+    arr = self.value_function_with_structure(value_function)
+    fig, ax = plt.subplots()
+    im = ax.imshow(np.reshape(arr, (-1, self.map_width-2)))
+    cax = fig.add_axes([ax.get_position().x1+0.01, ax.get_position().y0, 0.02,
+                        ax.get_position().height])
+    fig.colorbar(im, cax=cax)
+    plt.show()
+
+
+if __name__ == '__main__':
+
+  env = Environment(map_data_2)
+  env._get_next_state(10, Action.DOWN)
+  v = [0] * env.num_state
+  env.pretty_print_value_function(v)
+  env.plot_value_function(v)
---
+++ env_test.py
@@ -0,0 +1,62 @@
+import unittest
+from parameterized import parameterized
+import env
+CellType = env.CellType
+Action = env.Action
+
+test_map_data = '''\
+######
+#X   #
+# ## #
+# #  #
+# #  #
+#   X#
+######
+'''
+
+
+class TestEnv(unittest.TestCase):
+
+  def setUp(self) -> None:
+    self.env = env.Environment(test_map_data)
+
+  def test_init(self):
+    self.assertEqual(self.env.num_state, 16)
+    self.assertEqual(self.env.map_width, 6)
+    self.assertEqual(self.env.map_height, 7)
+
+  def test_symbol_to_cell_type(self):
+    self.assertEqual(env.symbol_to_cell_type('#'), CellType.WALL)
+    self.assertEqual(env.symbol_to_cell_type(' '), CellType.EMPTY_CELL)
+    self.assertEqual(env.symbol_to_cell_type('X'), CellType.GOAL_CELL)
+
+  @parameterized.expand([[0, (1, 1)], [1, (1, 2)]])
+  def test_state_to_map(self, state, map):
+    self.assertEqual(self.env.state_to_map[state], map)
+
+  @parameterized.expand([[1, Action.RIGHT, 2], 
+                         [3, Action.RIGHT, 3],
+                         [1, Action.LEFT, 0]])
+  def test_get_next_state(self, state, action, expected_next_state):
+    self.assertEqual(self.env._get_next_state(state, action),
+                     expected_next_state)
+
+  def test_get_next_state_for_goal_state(self):
+    self.assertEqual(self.env._get_next_state(0, Action.DOWN), 0)
+
+  @parameterized.expand([[1, Action.RIGHT, 2, 1.],
+                         [3, Action.RIGHT, 3, 1.],
+                         [8, Action.UP, 7, 0.]])
+  def test_get_transition_probability(self, state, action, next_state,
+                                      expected_probability):
+    self.assertEqual(
+      self.env.get_transition_probability(state, action, next_state),
+      expected_probability)
+
+  @parameterized.expand([[0, Action.RIGHT, 0.], [3, Action.RIGHT, -1.]])
+  def test_get_reward(self, state, action, expected_reward):
+    self.assertEqual(self.env.get_reward(state, action), expected_reward)
+
+
+if __name__ == '__main__':
+  unittest.main()
